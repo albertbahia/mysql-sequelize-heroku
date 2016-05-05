ec91179933df8afa09d14020175fc685a66f6610
albertbahia <albert.bahia1@gmail.com>
2016-05-05 15:58:30 -0400

    server config and added handlebars for templates

--- server.js
+++ server.js
@@ -1,5 +1,22 @@
 var express = require('express');
 var sequelize = require('sequelize');
 var mysql = require('mysql');
+var exphbs = require('express-handlebars');
 
+// Initiate Express app
 var app = express();
+
+// Setup Handlebars with Express
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+// Define Node port
+var PORT = process.env.PORT || 8000;
+
+app.get('/', function(request, response) {
+  response.render('home');
+});
+
+app.listen(PORT, function(err) {
+  console.log('Listening on port ' + PORT);
+})
