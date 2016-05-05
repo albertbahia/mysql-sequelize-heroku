9b00490a10c896bbd5bb6c61b672b5ec99519839
albertbahia <albert.bahia1@gmail.com>
2016-05-05 18:54:45 -0400

    added .env file

--- server.js
+++ server.js
@@ -2,13 +2,28 @@ var express = require('express');
 var mysql = require('mysql');
 var exphbs = require('express-handlebars');
 var bodyParser = require('body-parser');
-var Todo = require('./models/todo.js');
 
 // Initiate Express app
 var app = express();
 
 // Middleware
 app.use(bodyParser.urlencoded({extended: false}));
+
+// Database setup
+var Sequelize = require('sequelize');
+var connection = new Sequelize('todo_db', 'root', 'hacktheplanet', {
+  host: 'localhost',
+  dialect: 'mysql',
+  port: '3306'
+});
+
+var Todo = connection.define('todo', {
+  description: {
+    type: Sequelize.STRING,
+    field: 'description',
+    allowNull: false
+  }
+});
 
 // Setup Handlebars with Express
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
@@ -18,15 +33,28 @@ app.set('view engine', 'handlebars');
 var PORT = process.env.PORT || 8000;
 
 // Routes
-app.get('/', function(request, response) {
-  // Todo.findAll({}).then(function(todos) {
-  //   res.render('home', {todos})
-  // });
-  
-  response.render('home', {yerp: 'Hello World2!'});
+app.get('/', function(req, res) {
+  Todo.findAll({}).then(function(todos) {
+    res.render('home', {todos})
+  });
 });
 
-// Node port listener
-app.listen(PORT, function(err) {
-  console.log('Listening on port ' + PORT);
-})
+app.post('/addTodo', function(req,res) {
+  console.log(req.body);
+  var description = req.body.description;
+
+  Todo.create({
+    description: description
+  }).then(function(result) {
+    res.redirect('/');
+  }).catch(function(err) {
+    console.log('Error: ' + err);
+  });
+});
+
+// Node port listener with database connection while Express server is running
+connection.sync().then(function() {
+  app.listen(PORT, function(err) {
+    console.log('Listening on port ' + PORT);
+  });
+});
